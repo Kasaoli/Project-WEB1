68467c413fe149f7cefb4a7ff2e923fbeb584f01
kasaoli <kauansantos60@gmail.com>
2025-01-18 17:34:47 -0300

    Adicionar PainelFavoritos

--- src/componentes/CardAtleta.jsx
+++ src/componentes/CardAtleta.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../estilos/CardAtleta.css"
+import "../estilos/CardAtleta.css";
 
 const CardAtleta = ({ atleta, adicionarFavorito }) => {
     return (
@@ -9,7 +9,7 @@ const CardAtleta = ({ atleta, adicionarFavorito }) => {
             <p>{atleta.strTeam}</p>
             <button onClick={() => adicionarFavorito(atleta)}>Adicionar aos favoritos</button>
         </div>
-    )
-}
+    );
+};
 
 export default CardAtleta;
---
+++ src/componentes/PainelFavoritos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../estilos/PainelFavoritos.css";
+
+const PainelFavoritos = ({ favoritos }) => {
+    return (
+        <div className="painel-favoritos">
+            <h2>Favoritos</h2>
+            {favoritos.length === 0 ? (
+                <p>Nenhum atleta favorito ainda.</p>
+            ) : (
+                favoritos.map((atleta) => (
+                    <div key={atleta.idPlayer} className="favorito-card">
+                        <img src={atleta.strThumb || "https://via.placeholder.com/150"} alt={atleta.strPlayer} />
+                        <h3>{atleta.strPlayer}</h3>
+                        <p>{atleta.strTeam}</p>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
+
+export default PainelFavoritos;
