8f0c25f686884e35673bfc52390ac5fa99814d97
kasaoli <kauansantos60@gmail.com>
2025-01-18 17:02:01 -0300

    Adicionar ligação com a api

---
+++ src/api/esportesAPI.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_BASE = "https://www.thesportsdb.com/api/v1/json/2/searchplayers.php";
+
+export const buscarAtletas = async (nome) => {
+    const response = await axios.get(`${API_BASE}?p=${nome}`);
+    return response.data.player || [];
+};
