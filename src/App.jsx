55023b179f66d01192754f59a9b6391153b189c1
kasaoli <kauansantos60@gmail.com>
2025-01-18 18:21:58 -0300

    Modificar classes App e main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { buscarAtletas } from "./api/esportesAPI";
+import CardAtleta from "./componentes/CardAtleta";
+import PainelFavoritos from "./componentes/PainelFavoritos";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [nome, setNome] = useState("");
+  const [atletas, setAtletas] = useState([]);
+  const [favoritos, setFavoritos] = useState([]);
 
+  const handlePesquisa = async () => {
+    const resultados = await buscarAtletas(nome);
+    setAtletas(resultados);
+  };
+
+  const adicionarFavorito = (atleta) => {
+    if (!favoritos.find((fav) => fav.idPlayer === atleta.idPlayer)){
+      setFavoritos([...favoritos, atleta]);
+    }
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <h1>Pesquisa de Atletas</h1>
+      <input type="text" placeholder="Digite o nome do atleta" value={nome} onChange={(e) => setNome(e.target.value)}/>
+      <button onClick={handlePesquisa}>Pesquisar</button>
 
-export default App
+      <div className="resultados">
+        {atletas.map((atleta) => (
+          <CardAtleta key={atleta.idPlayer} atleta={atleta} adicionarFavorito={adicionarFavorito} />
+        ))}
+      </div>
+
+      <PainelFavoritos favoritos={favoritos} />
+    </div>
+  );
+};
+
+export default App;
--- src/api/esportesAPI.js
+++ src/api/esportesAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://www.thesportsdb.com/api/v1/json/2/searchplayers.php";
+const API_BASE = "https://www.thesportsdb.com/api/v1/json/3/searchplayers.php";
 
 export const buscarAtletas = async (nome) => {
     const response = await axios.get(`${API_BASE}?p=${nome}`);
