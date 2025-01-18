6d71f7c57f8f0efd011d8cb75facbe6eb2070bcd
kasaoli <kauansantos60@gmail.com>
2025-01-18 17:17:59 -0300

    Adicionar CardAtleta

---
+++ src/componentes/CardAtleta.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CardAtleta = ({ atleta, adicionarFavorito }) => {
+    return (
+        <div className="card-atleta">
+            <img src={atleta.strThumb || "https://via.placeholder.com/150"} alt={atleta.strPlayer} />
+            <h3>{atleta.strPlayer}</h3>
+            <p>{atleta.strTeam}</p>
+            <button onClick={() => adicionarFavorito(atleta)}>Adicionar aos favoritos</button>
+        </div>
+    )
+}
+
+export default CardAtleta;
